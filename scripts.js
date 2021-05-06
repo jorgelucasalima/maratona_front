e6e5ca72cf17b83b3a5b6ab4ea8f784ce98380ab
jorgelucasalima <jorgelucasalima@gmail.com>
2021-05-06 19:51:45 -0300

    criando incremento do form

--- scripts.js
+++ scripts.js
@@ -14,6 +14,65 @@ const Modal = {
             .querySelector('.modal-overlay')
             .classList.remove('active');
     }
-
-
 }
+
+const transaction = [
+    {
+        id: 1,
+        description: 'Cartão de Crédito',
+        amount: 20000,
+        date: '23/01/2021',
+    },
+
+    {
+        id: 2,
+        description: 'Internet',
+        amount: 50000,
+        date: '26/01/2021',
+    },
+
+    {
+        id: 3,
+        description: 'Luz',
+        amount: -10000,
+        date: '24/01/2021',
+    },
+
+]
+
+
+const Transaction = {
+    incomes() {
+        //somar as entradas
+    }
+
+    expenses() {
+        //somar as saidas
+    }
+
+
+    total() {
+        //Total = incomes - expenses    
+    }
+    
+}
+
+
+const DOM = {
+    innerHTMLTransaction(){
+
+
+        
+
+        const html = `
+        <tr>
+            <td class="description">Luz</td>
+            <td class="expense">- R$ 500,00</td>
+            <td class="date">23/04/2021</td>
+            <td><img src="./assets/minus.svg" alt="Remover dado"></td>
+        </tr>
+        `
+    }
+}
+
+
